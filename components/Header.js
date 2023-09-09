4ff23a6160e26c7d26646689544a2e8c33fb34f1
keipy27 <kevinandris27@gmail.com>
2023-09-09 20:23:28 +1200

    Header.js code implemented

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+
+    
+    return (
+        <header>
+        <div className="container header">
+            <div className="logo">
+            <h2>
+                Crypto <span className="--color-danger">X</span>
+            </h2>
+            </div>
+
+            <nav>
+            <ul>
+                <li>
+                <Link href={"/"}>Home</Link>
+                </li>
+                <li>
+                <Link href={"/about"}>About</Link>
+                </li>
+                <li className="--btn --btn-primary">
+                <Link href={"/"}>Get Started</Link>
+                </li>
+            </ul>
+            </nav>
+        </div>
+        </header>
+    );
+};
+
+export default Header;
